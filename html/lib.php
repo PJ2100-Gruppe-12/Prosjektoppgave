3b5f37a6c6f56c1bf9657dacee420153752d964f
olve123 <olve.husby@gmail.com>
2015-03-13 12:35:45 +0100

    lagt til en header location til register

--- html/lib.php
+++ html/lib.php
@@ -8,12 +8,13 @@ mysql_connect("localhost", "root", "");
 mysql_select_db("bookingSide");
 
 function login($username, $password) {
-
+	//query til databasen som henter henter ut bruker og sjekker om passordet er tilhørende den linjen
 	$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 
 	$result = mysql_query($query);
+	//henter array fra queryen 
 	$rows = mysql_fetch_assoc($result);
-
+	//hvis dette stemmer så starter man session, og sender deg til booking.php
 	if (!empty($rows)) {
 		$_SESSION['ul'] = $rows['id'];
 		header("Location: booking.php");
@@ -22,28 +23,33 @@ function login($username, $password) {
 		echo "Failure!";
 	}
 }
-
+//legger til en funksjon som bryter av session (ikke tilkoblet en database.)
 function logout() {
 	session_destroy();
 	header("Location: login.php");
 }
 
 function register($username, $password, $confirmPassword,$sn, $pn, $tlf, $email) {
+	//denne sjekker om pw stemmer med confirmpw
 	if ($password == $confirmPassword) {
-		
+		//querry til database for å legge til bruker info
 		$query = "INSERT INTO users (username, password, studentnr,pNr,tlf,email) 
 		VALUES ('$username', '$password', '$sn','$pn','$tlf','$email')";
 		$result = mysql_query($query);
 
 
 		echo $result;
+		//hvis result har data sucess
 		if ($result) {
 			echo "Success!";
+			header("Location: login.php");
 		}
+		//hvis ikke failure
 		else {
 			echo "Failure!";
 		}
 	}
+	//slutt på pw if == cpw
 	else {
 		echo "Passordene er ikke like...";
 	}
@@ -58,7 +64,7 @@ function getBooking($id) {
 		if(!$r) {
 			echo "die"; 
 		}
-		
+		//lager array
 		while ($row = mysql_fetch_assoc($r)) {
 			$rooms[] = $row;
 		}
@@ -80,6 +86,7 @@ function getBooking($id) {
 }
 
 function addBooking() {
+
 
 }
 
