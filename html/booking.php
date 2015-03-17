06753cdf73e15aa95cfa477a989834cd4b7ba55a
olve123 <olve.husby@gmail.com>
2015-03-17 10:39:47 +0100

    lagt til etasjer

--- html/booking.php
+++ html/booking.php
@@ -12,18 +12,24 @@ $rooms = getBooking("0");
 
 <?php
 
-		//Lager en separat link for hver id ($rooms[i]['id'])
 		
-	
-		echo '<a href = "booking.php?floor=1">1</a>';
 		
+		for ($i=1; $i < 7; $i++) { 
+			echo '<a href = "booking.php?floor='.$i.'">'.$i.'</a>';
+		}
+		
+		$userfloor=$_GET['floor'];
 
 		for ($i=0; $i < count($rooms); $i++) { 
 			//TODO sjekk hvilken etasje bruker vil ha. 1-6
-			if ($rooms[$i]['floor']==1) {
+
+			//Viser rom som er på etasje 1 TODO : endre floor id til en dynamisk link. 
+			if ($rooms[$i]['floor']==$userfloor) {
+			//Lager en separat link for hver id ($rooms[i]['id'])
 			echo '<a class="rom-link" href="rom.php?id='.$rooms[$i]['id'].'"><div class = "box" id = "rom'. $rooms[$i]['id'] 
 			.'" >'.$rooms[$i]['name'].'</div></a>';	
 			}
+
 		}
 		
 
