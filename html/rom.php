04dfaf6dcf650928726a79e5378471120bd00390
jugdan14 <jugedan14@student.westerdals.no>
2015-03-19 13:18:08 +0100

    stuff

--- html/rom.php
+++ html/rom.php
@@ -2,6 +2,10 @@
 	include 'lib.php';
 	include 'header.php';
 	include'nav.php';
+	//if (!empty($_POST)) {
+	//	addBooking(userID['un'], $_POST['pw'], $_POST['cpw'], $_POST['sn'], $_POST['pn'],$_POST['tlf'], $_POST['email']);
+	//}
+
 
 
 	$urlid = $_GET['id'];
@@ -18,12 +22,15 @@
 	$now = date("y-m-d h:i:s");
 	$timestamp = strtotime($now);
 	
+	//TODO sjekke om rommet er ledig på spesifikke tiden. denne lager boxer til alle tidene i løpet av en dag.
 	
 
-
+		
 	
 
 	
+
+	//TODO lage en selector til hvilken tid man skal bruke og submitter informasjonen til databasen.
 	
 	if (isset($_POST['time']) && isset($_POST['date'])) {
 		addBooking($_GET['id'],$_POST['time'],$_POST['date']);
@@ -56,6 +63,8 @@
 	</select>
 	
 	</form>
+
+ 
 	 
 <?php
 ?>
@@ -65,3 +74,18 @@
 </html>
 
 
+
+<?php
+// echo '<form class = "form1" name= "choise" method ="post">
+// 		<input type="date" name="date">
+// 		<select name="time">';
+// for ($i=0; $i < 10; $i++) { 
+// 	echo '<option value = "1"'.$i.':00:00>'."1".$i.'</option>';
+
+
+// }
+// echo '<input name = "submit" id= "submit" type="submit" value="SUBMIT">
+// 	</select>
+	
+// 	</form>';
+?>
