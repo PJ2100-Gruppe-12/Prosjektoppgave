2eaa10122e25d867961e3d090af8f6ecbcdb29fa
jugdan14 <jugedan14@student.westerdals.no>
2015-03-19 12:41:02 +0100

    bakground fix

--- html/header.php
+++ html/header.php
@@ -21,7 +21,6 @@ if (isset($_GET['lo']) && $_GET['lo'] == "true") {
 	<body>
 		<!-- BODY -->
 		<div id = "logo"><img src="../pic/logo.png"></div>
-		<div id = "bg"><img id = "bgd"src="../pic/bg_dobbel.png"></div>
 		<div id ="wrapper">
 			
 			
